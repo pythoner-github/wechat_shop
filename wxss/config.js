dc79593e31c69c6205ed7ee384ffc24c00dfc573
admin <admin@git.com>
2018-09-26 07:50:16 +0800

    update

--- wxss/config.js
+++ wxss/config.js
@@ -3,7 +3,7 @@
 // 此处主机域名是腾讯云解决方案分配的域名
 // 小程序后台服务解决方案：https://www.qcloud.com/solution/la
 
-var host = "pyqylq7r.qcloud.la"
+var host = "rhlhm0h9.ws.qcloud.la"
 
 var config = {
   // 下面的地址配合云端 Server 工作
