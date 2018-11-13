f54f179ec7737ccdfe0364314191e7c9efba0594
admin <admin@git.com>
2018-11-13 18:10:29 +0800

    update

--- wxss/pages/user/dingdan.js
+++ wxss/pages/user/dingdan.js
@@ -391,6 +391,126 @@ Page ({
     })
   },
 
+  // 确认收货并付款
+  recOrderAndPayOrderByWechat: function(e) {
+    var that = this;
+    var orderId = e.currentTarget.dataset.orderId;
+
+    wx.showModal({
+      title   : '提示',
+      content : '您确定已收到菜品了吗？',
+
+      success: function(res) {
+        var order_id = e.currentTarget.dataset.orderId;
+        var order_sn = e.currentTarget.dataset.ordersn;
+
+        if (!order_sn) {
+          wx.showToast({
+            title   : "订单异常!",
+            duration: 2000,
+          });
+
+          return false;
+        }
+
+        wx.request({
+          url   : app.d.apiUrl + 'Wxpay/wxpay',
+          data  : {
+            order_id: order_id,
+            order_sn: order_sn,
+            uid     : app.d.userId,
+          },
+          method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+          header: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },              // 设置请求的 header
+
+          success: function(res) {
+            console.log(res)
+
+            if (res.data.status == 1) {
+              var order = res.data.arr;
+
+              wx.requestPayment({
+                timeStamp : order.timeStamp,
+                nonceStr  : order.nonceStr,
+                package   : order.package,
+                signType  : 'MD5',
+                paySign   : order.paySign,
+
+                success: function(res) {
+                  wx.showToast({
+                    title   : "支付成功!",
+                    duration: 2000,
+                  });
+
+                  res.confirm && wx.request({
+                    url   : app.d.apiUrl + 'Order/orders_edit',
+                    method: 'post',
+                    data  : {
+                      id  : orderId,
+                      type: 'receive',
+                    },
+                    header: {
+                      'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+
+                    success: function(res) {
+                      // init data
+                      var status = res.data.status;
+
+                      if (status == 1) {
+                        wx.showToast({
+                          title   : '操作成功！',
+                          duration: 2000
+                        });
+
+                        that.loadOrderList();
+                      } else {
+                        wx.showToast({
+                          title   : res.data.err,
+                          duration: 2000
+                        });
+                      }
+                    },
+
+                    fail: function() {
+                      // fail
+                      wx.showToast({
+                        title   : '网络异常！',
+                        duration: 2000
+                      });
+                    }
+                  });
+                },
+
+                fail: function(res) {
+                  wx.showToast({
+                    title   : res,
+                    duration: 3000
+                  })
+                }
+              })
+            } else {
+              wx.showToast({
+                title   : res.data.err,
+                duration: 2000
+              });
+            }
+          },
+
+          fail: function (e) {
+            // fail
+            wx.showToast({
+              title   : '网络异常！',
+              duration: 2000
+            });
+          }
+        })
+      }
+    });
+  }
+
   /**
    * 调用服务器微信统一下单接口创建一笔微信预订单
    */
