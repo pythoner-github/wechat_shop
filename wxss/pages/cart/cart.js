43970cdda17652ffb2d93879b611c098db904006
pythoner-github <2113445097@qq.com>
2019-01-01 19:41:53 +0800

    购物车数量修改

--- wxss/pages/cart/cart.js
+++ wxss/pages/cart/cart.js
@@ -8,6 +8,7 @@ Page ({
     minusStatuses : ['disabled', 'disabled', 'normal', 'normal', 'disabled'],
     total         : 0,
     carts         : []
+    
   },
 
   bindMinus: function(e) {
@@ -70,6 +71,68 @@ Page ({
       }
     });
   },
+
+  bindManual: function (e) {
+    var that = this;
+
+    var index = parseInt(e.currentTarget.dataset.index);
+   // var num = that.data.carts[index].num;
+    var num = e.detail.value;
+
+    // 自增
+    // num++;
+    console.log(num);
+    var cart_id = e.currentTarget.dataset.cartid;
+
+    wx.request({
+      url: app.d.apiUrl + 'Shopping/up_cart',
+      method: 'post',
+      data: {
+        user_id: app.d.userId,
+        num: num,
+        cart_id: cart_id
+      },
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+
+      success: function (res) {
+        var status = res.data.status;
+
+        if (status == 1) {
+          // 只有大于一件的时候，才能normal状态，否则disable状态
+          var minusStatus = num <= 1 ? 'disabled' : 'normal';
+          // 购物车数据
+          var carts = that.data.carts;
+          carts[index].num = num;
+          // 按钮可用状态
+          var minusStatuses = that.data.minusStatuses;
+          minusStatuses[index] = minusStatus;
+
+          // 将数值与状态写回
+          that.setData({
+            minusStatuses: minusStatuses
+          });
+
+          that.sum();
+        } else {
+          wx.showToast({
+            title: '操作失败！',
+            duration: 2000
+          });
+        }
+      },
+
+      fail: function () {
+        // fail
+        wx.showToast({
+          title: '网络异常！',
+          duration: 2000
+        });
+      }
+    });
+  },
+
 
   bindPlus: function(e) {
     var that = this;
