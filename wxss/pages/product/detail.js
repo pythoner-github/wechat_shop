364f648bcea5385836a5362440a7faf4eb02e103
pythoner-github <2113445097@qq.com>
2019-01-05 18:19:40 +0800

    海报功能

--- wxss/pages/product/detail.js
+++ wxss/pages/product/detail.js
@@ -71,6 +71,46 @@ Page ({
     }.bind(this), 200)
   },
 
+  // 弹窗
+  setModalShare: function (e) {
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+
+    this.animation = animation
+    animation.translateY(300).step();
+
+    this.setData({
+      animationData: animation.export()
+    })
+
+    //if (e.currentTarget.dataset.status == 1) {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    //}
+
+    setTimeout(function () {
+      animation.translateY(0).step()
+
+      this.setData({
+        animationData: animation
+      })
+
+      if (e.currentTarget.dataset.status == 0) {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+  },
+
   // 加减
   changeNum: function  (e) {
     var that = this;
